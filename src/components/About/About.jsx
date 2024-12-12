50d1807dca93f3bf4c997a97695ddcf0c5c07226
Salman <salman26080@gmail.com>
2024-12-12 15:16:46 +0800

    Updating hero and about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,30 +17,27 @@ export const About = () => {
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
             <div className={styles.aboutItemText}>
-              <h3>Frontend Developer</h3>
+              <h3>Machine Learning Engineer</h3>
               <p>
-                I'm a frontend developer with experience in building responsive
-                and optimized sites
+                I have experience in building and optimizing deep learning models.
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
             <div className={styles.aboutItemText}>
-              <h3>Backend Developer</h3>
+              <h3>Data Scientist</h3>
               <p>
-                I have experience developing fast and optimised back-end systems
-                and APIs
+                I have experience in doing data analysis and forecasting.
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
             <div className={styles.aboutItemText}>
-              <h3>UI Designer</h3>
+              <h3>Frontend Engineer</h3>
               <p>
-                I have designed multiple landing pages and have created design
-                systems as well
+                I have developed frontend for many websites and projects.
               </p>
             </div>
           </li>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Salman</h1>
         <p className={styles.description}>
-          Second-year NUS student pursuing a double major in Data Science & Analytics and Computer Science, 
+          A second-year NUS student pursuing a double major in Data Science & Analytics and Computer Science, 
           with a minor in Quantitative Finance. Passionate about A.I. and machine learning.
         </p>
         <a href="mailto:salman26080@gmail.com" className={styles.contactBtn}>
