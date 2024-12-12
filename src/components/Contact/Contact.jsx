b1ba02aa555f992114bf24eec8a73b84caff39a2
Salman <salman26080@gmail.com>
2024-12-12 15:22:46 +0800

    Modified the contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,18 +13,18 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="mailto:salman26080@gmail.com">salman26080@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/muhammadsalmanalfarisi1/">linkedin.com/muhammadsalmanalfarisi1</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a href="https://github.com/SalmanAlfarisi5">github.com/SalmanAlfarisi5</a>
         </li>
       </ul>
     </footer>
