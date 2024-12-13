587490835bba7d8181adaaf788c635aa837d0378
Salman <salman26080@gmail.com>
2024-12-13 09:31:05 +0800

    Adding courses page

---
+++ src/components/Course/Course.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./Course.module.css";
+//import courses from "../../data/courses.json";
+//import { getImageUrl } from "../../utils";
+
+export const Course = () => {
+  return (
+    <section className={styles.container} id="experience">
+      <h2 className={styles.title}>Courses</h2>
+    </section>
+  );
+};
