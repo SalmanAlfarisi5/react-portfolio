5cc35792b0379544d20a74439ceca955f1559a9a
Salman <salman26080@gmail.com>
2024-12-13 10:30:57 +0800

    Updated Courses page

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 
 import styles from "./Course.module.css";
-//import courses from "../../data/courses.json";
-//import { getImageUrl } from "../../utils";
+import courses from "../../data/courses.json";
 
 export const Course = () => {
   return (
     <section className={styles.container} id="experience">
-      <h2 className={styles.title}>Courses</h2>
+      <h2 className={styles.title}>Relevant Courses</h2>
+      <div className={styles.content}>
+      <ul className={styles.courses}>
+          {courses.map((course, id) => {
+            return (
+              <li key={id} className={styles.course}>
+                <a
+                  href={`https://nusmods.com/courses/${course.name}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.courseLink}
+                >
+                  <div className={styles.courseItemDetails}>
+                    <h3>{course.name}</h3>
+                    <p>{course.description}</p>
+                  </div>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </section>
   );
 };
